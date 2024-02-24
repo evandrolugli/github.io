3227ed5fb7635ba36e6e602ddce18f6ebef705c6
Evandro Lugli <evandro.lugli@gmail.com>
2024-02-24 22:34:58 +1100

    testing

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
+    loadContent("partials/about.html"); // Load default content
+
+    // Add event listeners for navigation
+    document.querySelectorAll("nav a").forEach(function (link) {
+        link.addEventListener("click", function (event) {
+            event.preventDefault();
+            const targetPartial = this.getAttribute("href").substring(1) + ".html";
+            loadContent("partials/" + targetPartial);
+        });
+    });
+
 
     const mobileMenuButton = document.getElementById('mobile-menu');
     const menuList = document.getElementById('menu');
@@ -8,11 +19,11 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-function loadPage(page) {
-    fetch(`partials/${page}`)
+function loadContent(url) {
+    fetch(url)
         .then(response => response.text())
         .then(data => {
-            document.getElementById('main-content').innerHTML = data;
+            document.getElementById("main-content").innerHTML = data;
         })
-        .catch(error => console.error('Error loading page:', error));
+        .catch(error => console.error("Error loading content:", error));
 }
